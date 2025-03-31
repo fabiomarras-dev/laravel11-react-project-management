edd4897b355b880447a2fe8dd902fe0df578c459
todoketee <fabio-marras@hotmail.com>
2025-03-31 14:53:37 +0200

    task table part 1

--- laravel11-react-project-management/resources/js/Pages/Project/Show.jsx
+++ laravel11-react-project-management/resources/js/Pages/Project/Show.jsx
@@ -17,15 +17,17 @@ export default function Show({ project }) {
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6  lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
 
-                            <div>
-                                <img
-                                    src={project.image_path}
-                                    alt=""
-                                    className="w-full h-64 object-cover"
-                                />
-                            </div>
+                        <div>
+                            <img
+                                src={project.image_path}
+                                alt=""
+                                className="w-full h-64 object-cover"
+                            />
+                        </div>
+
+
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
 
                             <div className="grid gap-1 grid-cols-2 mt-2">
                                 <div>
@@ -50,10 +52,52 @@ export default function Show({ project }) {
                                         </p>
                                     </div>
 
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Created By</label>
+                                        <p className="mt-1">{project.createdBy.name}</p>
+                                    </div>
+
                                 </div>
+
+                                <div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Due Date</label>
+                                        <p className="mt-1">{project.due_date}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Create Date</label>
+                                        <p className="mt-1">{project.created_at}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Updated By</label>
+                                        <p className="mt-1">{project.updatedBy.name}</p>
+                                    </div>
+                                </div>
+                                    
+                            </div>
+
+                            <div className="mt-4">
+                                <label className="font-bold text-lg">Project Description</label>
+                                <p className="mt-1">{project.description}</p>
                             </div>
 
                         </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6  lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Table place         
+
+                        </div>
+
                     </div>
                 </div>
             </div>
