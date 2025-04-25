734e782e71976841f9dcdb12a7633ab341e9658c
todoketee <fabio-marras@hotmail.com>
2025-04-25 15:12:04 +0200

    dashboard section part 2

--- laravel11-react-project-management/app/Http/Controllers/DashboardController.php
+++ laravel11-react-project-management/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -19,6 +20,10 @@ class DashboardController extends Controller
         $totalCompletedTasks = Task::query()->where('status', 'completed')->count();
         $myCompletedTasks = Task::query()->where('status', 'completed')->where('assigned_user_id', $user->id)->count();
 
-        return inertia('Dashboard', compact('totalPendingTasks', 'myPendingTasks', 'totalProgressTasks', 'myProgressTasks', 'totalCompletedTasks', 'myCompletedTasks'));
+        $activeTasks = Task::query()->whereIn('status', ['pending', 'in_progress'])->where('assigned_user_id', $user->id)->limit(10)->get();
+
+        $activeTasks = TaskResource::collection($activeTasks);
+
+        return inertia('Dashboard', compact('totalPendingTasks', 'myPendingTasks', 'totalProgressTasks', 'myProgressTasks', 'totalCompletedTasks', 'myCompletedTasks', 'activeTasks'));
     }
 }
--- laravel11-react-project-management/resources/js/Pages/Dashboard.jsx
+++ laravel11-react-project-management/resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,8 @@
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from '@/constants';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
-export default function Dashboard({ auth, totalPendingTasks, myPendingTasks, totalProgressTasks, myProgressTasks, totalCompletedTasks, myCompletedTasks }) {
+export default function Dashboard({ auth, totalPendingTasks, myPendingTasks, totalProgressTasks, myProgressTasks, totalCompletedTasks, myCompletedTasks, activeTasks }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -45,6 +46,55 @@ export default function Dashboard({ auth, totalPendingTasks, myPendingTasks, tot
                                 /
                                 <span className='ml-2'>{totalCompletedTasks}</span>
                             </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 grid mt-4">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <h3 className='text-grey-200 text-2xl font-semibold'>My Active Tasks</h3>
+                            
+                            <table className="mt-3 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr>
+                                        <th className="px-3 py-3">ID</th>
+                                        <th className="px-3 py-3">Project Name</th>
+                                        <th className="px-3 py-3">Name</th>
+                                        <th className="px-3 py-3">Status</th>
+                                        <th className="px-3 py-3">Due Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {activeTasks.data.map((task) => (
+                                    <tr key={task.id}>
+                                    <td className="px-3 py-2">{task.id}</td>
+                                    <td className="px-3 py-2 text-white hover:underline">
+                                        <Link href={route("project.show", task.project.id)}>
+                                        {task.project.name}
+                                        </Link>
+                                    </td>
+                                    <td className="px-3 py-2 text-white hover:underline">
+                                        <Link href={route("task.show", task.id)}>
+                                        {task.name}
+                                        </Link>
+                                    </td>
+                                    <td className="px-3 py-2">
+                                        <span
+                                        className={
+                                            "px-2 py-1 rounded text-nowrap text-white " +
+                                            TASK_STATUS_CLASS_MAP[task.status]
+                                        }
+                                        >
+                                        {TASK_STATUS_TEXT_MAP[task.status]}
+                                        </span>
+                                    </td>
+                                    <td className="px-3 py-2 text-nowrap">{task.due_date}</td>
+                                    </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
                         </div>
                     </div>
                 </div>
